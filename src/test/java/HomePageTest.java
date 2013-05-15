ed59b5f3f82447aa7c3889edb1643c5a817d15af
Xavier Hanin <xavier.hanin@gmail.com>
2013-05-15 07:55:06 +0200

    add signup form

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,12 +1,29 @@
+import org.fluentlenium.core.domain.FluentWebElement;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.fest.assertions.Assertions.assertThat;
 
 public class HomePageTest extends AbstractPageTest {
-  @Test
-  public void should_display_homepage() throws InterruptedException {
-    goTo("/");
+    @Test
+    public void should_display_homepage() throws InterruptedException {
+      goTo("/");
 
-    assertThat(title()).isEqualTo("CodeStory - Geektic");
-  }
+      assertThat(title()).isEqualTo("CodeStory - Geektic");
+
+        FluentWebElement searchInput = findFirst("#search input");
+
+        assertThat(searchInput.getText()).isEmpty();
+    }
+
+    @Test
+    public void should_update_geeks_on_search() throws InterruptedException {
+      goTo("/");
+
+        assertThat(find(".resultSquare.shown")).isEmpty();
+        fill("#search input").with("test");
+
+        await().atMost(5, TimeUnit.SECONDS).until(".resultSquare.shown").hasSize(1);
+    }
 }
--- web/controllers.coffee
+++ web/controllers.coffee
@@ -8,4 +8,20 @@ homeController = ($scope, $http, $location) ->
       $scope.geeks = json
 
   $scope.search = $location.search().q || ''
+
+  $scope.geek =
+    prenom: ''
+    nom: ''
+    like1: ''
+    like2: ''
+    like3: ''
+
+  $scope.mode = 'default'
+
+  $scope.setMode = (mode) ->
+    $scope.mode = mode
+
+  $scope.inscription = (geek) ->
+    console.log 'inscription', geek
+
   $scope.refresh()
