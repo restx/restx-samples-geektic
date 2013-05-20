88967aebed94485cd0575e62c2a0b543aa00fb95
Xavier Hanin <xavier.hanin@gmail.com>
2013-05-20 23:51:12 +0200

    use RestxSpecRule to run selenium tests
    so that we can benefit from Mongo test DB and collection fixtures

--- src/test/java/AbstractPageTest.java
+++ src/test/java/AbstractPageTest.java
@@ -1,43 +1,21 @@
-import main.MainGeekticServer;
 import misc.PhantomJsTest;
-import org.junit.Before;
-
-import java.util.Random;
+import org.junit.ClassRule;
+import restx.server.WebServer;
+import restx.server.WebServerSupplier;
+import restx.server.simple.simple.SimpleWebServer;
+import restx.tests.RestxSpecRule;
 
 public abstract class AbstractPageTest extends PhantomJsTest {
-  private static final int TRY_COUNT = 10;
-  private static final int DEFAULT_PORT = 8183;
-  private static Random RANDOM_PORT = new Random();
+    @ClassRule
+    public static RestxSpecRule rule = new RestxSpecRule("", new WebServerSupplier() {
+        @Override
+        public WebServer newWebServer(int port) {
+            return new SimpleWebServer("", "", port);
+        }
+    }, RestxSpecRule.defaultFactory());
 
-  private static int port;
-  private static MainGeekticServer server;
-
-  @Before
-  public void startServerOnce() {
-    if (server != null) {
-      return;
+    @Override
+    protected String defaultUrl() {
+        return rule.getServer().baseUrl();
     }
-
-    for (int i = 0; i < TRY_COUNT; i++) {
-      try {
-        port = randomPort();
-        server = new MainGeekticServer();
-        server.start(port);
-        return;
-      } catch (Exception e) {
-        System.err.println("Unable to bind server: " + e);
-      }
-    }
-    throw new IllegalStateException("Unable to start server");
-  }
-
-
-  private synchronized int randomPort() {
-    return DEFAULT_PORT + RANDOM_PORT.nextInt(1000);
-  }
-
-  @Override
-  protected String defaultUrl() {
-    return "http://localhost:" + port;
-  }
 }
--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,5 +1,10 @@
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.fluentlenium.core.domain.FluentWebElement;
 import org.junit.Test;
+import restx.specs.RestxSpec;
+import restx.specs.mongo.GivenJongoCollection;
+import restx.tests.WhenThenCheck;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,45 +13,78 @@ import static org.fest.assertions.Assertions.assertThat;
 public class HomePageTest extends AbstractPageTest {
     @Test
     public void should_display_homepage() throws InterruptedException {
-      goTo("/");
+        runTestWithGeeks("{ \"_id\" : { \"$oid\" : \"519526f5207108cdfa4d0cc0\"} , " +
+                "\"nom\" : \"Doe\" , \"prenom\" : \"John\" , " +
+                "\"email\" : \"xav@acme.com\" , \"likes\" : [ \"test\"]}",
+                new WhenThenCheck() {
+                    @Override
+                    protected void check(ImmutableMap<String, String> params) {
+                        goTo("/");
 
-      assertThat(title()).isEqualTo("CodeStory - Geektic");
+                        assertThat(title()).isEqualTo("CodeStory - Geektic");
 
-        FluentWebElement searchInput = findFirst("#search input");
+                        FluentWebElement searchInput = findFirst("#search input");
 
-        assertThat(searchInput.getText()).isEmpty();
+                        assertThat(searchInput.getText()).isEmpty();
+                    }
+                });
     }
 
     @Test
     public void should_update_geeks_on_search() throws InterruptedException {
-      goTo("/");
+        runTestWithGeeks("{ \"_id\" : { \"$oid\" : \"519526f5207108cdfa4d0cc0\"} , " +
+                        "\"nom\" : \"Doe\" , \"prenom\" : \"John\" , " +
+                        "\"email\" : \"xav@acme.com\" , \"likes\" : [ \"test\"]}",
+                new WhenThenCheck() {
+                    @Override
+                    protected void check(ImmutableMap<String, String> params) {
+                        goTo("/");
 
-        assertThat(find(".resultSquare.shown")).isEmpty();
-        fill("#search input").with("test");
+                        assertThat(find(".resultSquare.shown")).isEmpty();
+                        fill("#search input").with("test");
 
-        await().atMost(5, TimeUnit.SECONDS).until(".resultSquare.shown").hasSize(1);
+                        await().atMost(5, TimeUnit.SECONDS).until(".resultSquare.shown").hasSize(1);
+                    }
+                });
     }
-
 
     @Test
     public void should_signup_geek() throws InterruptedException {
-      goTo("/");
+        runTestWithGeeks("{ \"_id\" : { \"$oid\" : \"519526f5207108cdfa4d0cc0\"} , " +
+                        "\"nom\" : \"Doe\" , \"prenom\" : \"John\" , " +
+                        "\"email\" : \"xav@acme.com\" , \"likes\" : [ \"test\"]}",
+                new WhenThenCheck() {
+                    @Override
+                    protected void check(ImmutableMap<String, String> params) {
+                        goTo("/");
 
-        find("#showSignupBtn").click();
+                        find("#showSignupBtn").click();
 
-        fill(".signup.form .prenom").with("John");
-        fill(".signup.form .nom").with("Doe");
-        fill(".signup.form .email").with("test@acme.com");
-        fill(".signup.form .like1").with("signuptest");
+                        fill(".signup.form .prenom").with("John");
+                        fill(".signup.form .nom").with("Doe");
+                        fill(".signup.form .email").with("test@acme.com");
+                        fill(".signup.form .like1").with("signuptest");
 
-        find(".signup.form .inscription").click();
+                        find(".signup.form .inscription").click();
 
-        await().atMost(5, TimeUnit.SECONDS).until("#intro .user h3").containsText("John Doe");
+                        await().atMost(5, TimeUnit.SECONDS).until("#intro .user h3").containsText("John Doe");
 
-        fill("#search input").with("signuptest");
+                        fill("#search input").with("signuptest");
 
-        await().atMost(5, TimeUnit.SECONDS).until(".resultSquare.shown").hasSize(1);
+                        await().atMost(5, TimeUnit.SECONDS).until(".resultSquare.shown").hasSize(1);
 
-        assertThat(find(".resultSquare.shown img").first().getAttribute("src")).isEqualTo("http://gravatar.com/avatar/cd39cb289b96854268bbdd63908230ab?s=256");
+                        assertThat(find(".resultSquare.shown img").first().getAttribute("src")).isEqualTo("http://gravatar.com/avatar/cd39cb289b96854268bbdd63908230ab?s=256");
+                    }
+                });
+    }
+
+
+    private void runTestWithGeeks(String geeks, WhenThenCheck check) {
+        rule.runTest(new RestxSpec("test",
+                ImmutableList.<RestxSpec.Given>of(new GivenJongoCollection("geeks", "",
+                        geeks,
+                        ImmutableList.<String>of())),
+                ImmutableList.<RestxSpec.When>of(check)
+        ));
     }
 }
